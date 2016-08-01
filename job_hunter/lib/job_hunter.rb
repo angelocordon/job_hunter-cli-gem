5fdeac298ac7f6fb8be4e52d6b9df5feab563fcc
Lenn Hypolite <lenntriniman@hotmail.com>
2016-08-01 21:28:16 +0000

    changed require to require_relative

--- job_hunter/lib/job_hunter.rb
+++ job_hunter/lib/job_hunter.rb
@@ -2,10 +2,10 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
-require "../lib/job_hunter/scraper"
-require "../lib/job_hunter/version"
-require "../lib/job_hunter/jobs"
-require "../lib/job_hunter/cli"
+require_relative "job_hunter/scraper"
+require_relative "job_hunter/version"
+require_relative "job_hunter/cli"
+require_relative "job_hunter/jobs"
 
 #this sets up the JobHunter Module as having access to all the JobHunter::*FILES*
 module JobHunter
